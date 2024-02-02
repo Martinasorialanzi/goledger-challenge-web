cc5570eb80d069d032e1bf9cc6a4cee92b59d1ca
martinasorialanzi <martinasorialanzi@gmail.com>
2024-02-02 11:53:49 -0300

    page 404

---
+++ client/src/app/not-found.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="global">
+   <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <h2 className="text-6xl font-bold mt-6">404</h2>
+        <p className="text-xl my-4">The page is not done yet!</p>
+        
+          <a  href="/" className=" my-6 text-white">Return Home</a>
+       
+      </div>
+    </div>
+    </div>
+  );
+}
